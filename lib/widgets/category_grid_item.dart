0379f946219f2b902e80a91a7035a694467e64c7
Nathnael <nathnaelzelalem@gmail.com>
2025-02-11 16:09:12 +0300

    Widgets and Screen

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app_flutter/models/category.dart';
+
+class categoryGridItem extends StatelessWidget {
+  const categoryGridItem({super.key, required this.category});
+
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
